01ab046a57cbe1bbd7ff624982f20905f5f0aa6f
Dmytro Vasylkov <dmytro.vasylkov@appsflyer.com>
2021-07-23 00:59:11 +0300

    import

--- oemsdk/src/test/java/com/appsflyer/oem/HashUtilsTest.kt
+++ oemsdk/src/test/java/com/appsflyer/oem/HashUtilsTest.kt
@@ -1,13 +1,12 @@
 package com.appsflyer.oem
 
-import com.appsflyer.oem.HashUtils.hmac
 import org.junit.Assert
 import org.junit.Test
 
 class HashUtilsTest {
     @Test
     fun hmac() {
-        val actual = hmac("pizza", "bitcoin")
+        val actual = HashUtils.hmac("pizza", "bitcoin")
         @Suppress("SpellCheckingInspection")
         Assert.assertEquals(
             "368b0a0c673945ce5b87f3afffdff90f8bf41ffff06be1ed394fc54a4458dc94",
