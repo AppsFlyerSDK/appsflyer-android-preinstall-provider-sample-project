d49d241244e91d400d3f828b9c443e68728a6ba4
Dmytro Vasylkov <dmytro.vasylkov@appsflyer.com>
2021-07-23 00:58:39 +0300

    rename

--- oemsdk/src/test/java/com/appsflyer/oem/HashUtilsTest.kt
+++ oemsdk/src/test/java/com/appsflyer/oem/HashUtilsTest.kt
@@ -7,11 +7,11 @@ import org.junit.Test
 class HashUtilsTest {
     @Test
     fun hmac() {
-        val hmacActual = hmac("pizza", "bitcoin")
+        val actual = hmac("pizza", "bitcoin")
         @Suppress("SpellCheckingInspection")
         Assert.assertEquals(
             "368b0a0c673945ce5b87f3afffdff90f8bf41ffff06be1ed394fc54a4458dc94",
-            hmacActual
+            actual
         )
     }
 }
